aa037e3fe3bdc94427e361f0cb2d0803e752e7d5
iamtatsuki05 <tatsukio0522@gmail.com>
2025-09-01 19:36:16 +0900

    test

--- tests/nlp/constract_llm/eval/sentence_model/test_metric.py
+++ tests/nlp/constract_llm/eval/sentence_model/test_metric.py
@@ -1,30 +1,30 @@
-# import pytest
-# import torch
+import pytest
+import torch
 
-# from nlp.constract_llm.eval.sentence_model.metric import compute_alignment, compute_uniformity
-
-
-# @pytest.mark.parametrize(
-#     'z1, z2, expected',
-#     [
-#         # Two identical vectors → alignment=0
-#         (torch.tensor([[1.0, 0.0], [0.0, 1.0]]), torch.tensor([[1.0, 0.0], [0.0, 1.0]]), 0.0),
-#         # Orthogonal vectors → alignment=2
-#         (torch.tensor([[1.0, 0.0]]), torch.tensor([[0.0, 1.0]]), 2.0),
-#     ],
-# )
-# def test_compute_alignment_param(z1, z2, expected):
-#     assert abs(compute_alignment(z1, z2) - expected) < 1e-6
+from nlp.constract_llm.eval.sentence_model.metric import compute_alignment, compute_uniformity
 
 
-# @pytest.mark.parametrize(
-#     'z, t, expected',
-#     [
-#         # Two identical vectors → distance=0, uniformity=log(exp(0))=0
-#         (torch.tensor([[1.0, 0.0], [1.0, 0.0]]), 2.0, 0.0),
-#         # Orthogonal vectors → distance=2, uniformity=log(exp(-2*2))= -4
-#         (torch.tensor([[1.0, 0.0], [0.0, 1.0]]), 2.0, -4.0),
-#     ],
-# )
-# def test_compute_uniformity_param(z, t, expected):
-#     assert abs(compute_uniformity(z, t=t) - expected) < 1e-6
+@pytest.mark.parametrize(
+    'z1, z2, expected',
+    [
+        # Two identical vectors → alignment=0
+        (torch.tensor([[1.0, 0.0], [0.0, 1.0]]), torch.tensor([[1.0, 0.0], [0.0, 1.0]]), 0.0),
+        # Orthogonal vectors → alignment=2
+        (torch.tensor([[1.0, 0.0]]), torch.tensor([[0.0, 1.0]]), 2.0),
+    ],
+)
+def test_compute_alignment_param(z1, z2, expected):
+    assert abs(compute_alignment(z1, z2) - expected) < 1e-6
+
+
+@pytest.mark.parametrize(
+    'z, t, expected',
+    [
+        # Two identical vectors → distance=0, uniformity=log(exp(0))=0
+        (torch.tensor([[1.0, 0.0], [1.0, 0.0]]), 2.0, 0.0),
+        # Orthogonal vectors → distance=2, uniformity=log(exp(-2*2))= -4
+        (torch.tensor([[1.0, 0.0], [0.0, 1.0]]), 2.0, -4.0),
+    ],
+)
+def test_compute_uniformity_param(z, t, expected):
+    assert abs(compute_uniformity(z, t=t) - expected) < 1e-6
