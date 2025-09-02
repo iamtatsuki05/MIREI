f6431fa6542d6959545edc37b3a2191be9492085
iamtatsuki05 <tatsukio0522@gmail.com>
2025-09-03 01:20:14 +0900

    update config

--- scripts/constract_llm/eval/ft/run_jmteb.sh
+++ scripts/constract_llm/eval/ft/run_jmteb.sh
@@ -6,5 +6,17 @@ poetry install
 
 poetry run python -m jmteb \
     --embedder SentenceBertEmbedder \
-    --embedder.model_name_or_path "iamtatsuki05/Sentence-ModernBERT-JP-1.4B" \
-    --save_dir "output/Sentence-ModernBERT-JP-1.4B"
+    --embedder.model_name_or_path "iamtatsuki05/Sentence-Sarashina-Bi-0.5B" \
+    --save_dir "output/Sentence-Sarashina-Bi-0.5B"
+
+
+poetry run python -m jmteb \
+    --embedder SentenceBertEmbedder \
+    --embedder.model_name_or_path "iamtatsuki05/Sentence-ModernBERT-JP-0.5B" \
+    --save_dir "output/Sentence-ModernBERT-JP-0.5B"
+
+
+poetry run python -m jmteb \
+    --embedder SentenceBertEmbedder \
+    --embedder.model_name_or_path "iamtatsuki05/Sentence-Llama-Bi-JP-0.5B" \
+    --save_dir "output/Sentence-Llama-Bi-JP-0.5B"
