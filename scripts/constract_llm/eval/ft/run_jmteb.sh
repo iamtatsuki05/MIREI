2168d9cab1d57914e3c904e02700cf33997f1c82
iamtatsuki05 <tatsukio0522@gmail.com>
2025-09-03 14:27:48 +0900

    update model

--- scripts/constract_llm/eval/ft/run_jmteb.sh
+++ scripts/constract_llm/eval/ft/run_jmteb.sh
@@ -5,18 +5,18 @@ rm poetry.lock
 poetry install
 
 poetry run python -m jmteb \
-    --embedder SentenceBertEmbedder \
+    --embedder DataParallelSentenceBertEmbedder \
     --embedder.model_name_or_path "iamtatsuki05/Sentence-Sarashina-Bi-0.5B" \
     --save_dir "output/Sentence-Sarashina-Bi-0.5B"
 
 
 poetry run python -m jmteb \
-    --embedder SentenceBertEmbedder \
+    --embedder DataParallelSentenceBertEmbedder \
     --embedder.model_name_or_path "iamtatsuki05/Sentence-ModernBERT-JP-0.5B" \
     --save_dir "output/Sentence-ModernBERT-JP-0.5B"
 
 
 poetry run python -m jmteb \
-    --embedder SentenceBertEmbedder \
+    --embedder DataParallelSentenceBertEmbedder \
     --embedder.model_name_or_path "iamtatsuki05/Sentence-Llama-Bi-JP-0.5B" \
     --save_dir "output/Sentence-Llama-Bi-JP-0.5B"
